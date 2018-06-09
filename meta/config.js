ad147402868998914dc65a4f1d76c490bfb5c2d3
Darin Levesque <darinlevesque@gmail.com>
2018-06-08 22:49:51 -0400

    rewording

---
+++ meta/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Farmlify', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://farmlify.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'The modern way to bring local produce into your mouth', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'farmlify', // Disqus shortname.
+  userName: 'farmlify',
+  userTwitter: 'farmlify',
+  userLocation: '',
+  userDescription: '',
+  copyright: 'Copyright © Farmlify 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
