16ec9ac470f0d02dff878b090ff9209834082a47
Darin Levesque <darin@darinlevesque.com>
2018-06-12 14:53:55 -0400

    Removed tawk.to info

--- src/html.js
+++ src/html.js
@@ -39,18 +39,6 @@ export default class HTML extends Component {
           />
           {this.props.postBodyComponents}
           <script src={__PATH_PREFIX__ + '/js/toggle.js'} />
-
-          <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/5b1df5573604f81d726beab2/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          </script>
         </body>
       </html>
     )
